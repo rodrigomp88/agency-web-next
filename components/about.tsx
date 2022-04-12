c9318a3f1cf0fea1fa080a2fce5adfad59d9b2b5
Rodrigo <rodrigomp88@gmail.com>
2022-04-11 21:58:41 -0300

    about skills

---
+++ components/about.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Member from "./member";
+
+const About: React.FC = () => {
+  return (
+    <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
+      <div className="container mx-auto px-11">
+        <p className="leading-tight max-w-5xl mx-auto text-3xl lg:text-4xl tracking-tight">
+          <strong>Te ayudaremos a hacer WebApps mas rapido y mejores.</strong>
+          Nuestro equipo de expertos e ingenieros ha creado la mejor experiencia
+          de usuario de algunas de las WebApps mas populares de todo internet
+        </p>
+      </div>
+      <div className="container mx-auto px-21 text-center mt-28">
+        <h2>Nuestro Equipo</h2>
+        <div className="mt-2">Los &ldquo;operaciones especiales&rdquo;</div>
+        <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-2">
+          <Member
+            id="rodrigo"
+            name="rodrigo"
+            socialId="@rodrigomp88"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="david"
+            name="david"
+            socialId="@david"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="daniel"
+            name="daniel"
+            socialId="@daniel"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="raul"
+            name="raul"
+            socialId="@raul"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="pablo"
+            name="pablo"
+            socialId="@pablo"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="francisco"
+            name="francisco"
+            socialId="@francisco"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="diego"
+            name="diego"
+            socialId="@diego"
+            link="https://github.com/rodrigomp88"
+          />
+          <Member
+            id="egberto"
+            name="egberto"
+            socialId="@egberto"
+            link="https://github.com/rodrigomp88"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,32 +1,72 @@
-import React from "react"
-import Image from "next/image"
+import React, { useRef, useContext, useState, useCallback } from "react";
+import Image from "next/image";
+import { ScrollContext } from "../utils/scroll-observer";
 
 const Masthead: React.FC = () => {
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <video autoPlay loop muted playsInline className="absolute w-full h-full object-cover">
-                <source src="assets/masthead-bg.mp4" type="video/mp4; codecs=hvc1"/>
-                <source src="assets/masthead-bg.webm" type="video/webm; codecs=vp9"/>
-            </video>
-            <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
-                <Image src="/assets/logo.svg" width={128 / 3} height={144 / 3} alt="logo"/>
-            </div>
-            <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
-                <h1 className="mb-6 text-4xl xl:text-5xl">Agency</h1>
-                <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
-                    <span>App Development,</span> <span>done right.</span>
-                </h2>
-            </div>
-            <div className="flex-grow-0 pb-20 md:pb-10 transition-all duration-1000">
-                <Image 
-                src="/assets/arrow-down.png"
-                width={188 / 3}
-                height={105 / 3}
-                alt="scroll down"
-                />
-            </div>
-        </div>
-    )
-}
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const refContainer = useRef<HTMLDivElement>(null);
+  const { scrollY } = useContext(ScrollContext);
 
-export default Masthead
+  let progress = 0;
+
+  const { current: elContainer } = refContainer;
+  if (elContainer) {
+    // Calcula el progreso basado en la posicion de scroll
+    progress = Math.min(1, scrollY / elContainer.clientHeight);
+  }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true);
+  }, []);
+
+  return (
+    <div
+      ref={refContainer}
+      className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
+      style={{ transform: `translateY(-${progress * 20}vh)` }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover"
+      >
+        <source src="/assets/masthead-bg.mp4" type="video/mp4; codecs=hvc1" />
+        <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
+      </video>
+      <div
+        className={`flex-grow-0 pt-10 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] transition-opacity duration-1000`}
+      >
+        <Image
+          src="/assets/logo.svg"
+          width={124 / 3}
+          height={144 / 3}
+          alt="logo"
+        />
+      </div>
+      <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
+        <h1 className="mb-6 text-4xl xl:text-5xl">Modern Agency.</h1>
+        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
+          <span>WebApp Desarrollada,</span>{" "}
+          <span>with Nextjs, tailwindcss.</span>
+        </h2>
+      </div>
+      <div
+        className={`flex-grow-0 drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] pb-20 md:pb-10 transition-all duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+        }`}
+      >
+        <Image
+          src="/assets/arrow-down.png"
+          width={210 / 3}
+          height={90 / 3}
+          alt="scroll down"
+          onLoad={handleImageLoaded}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Masthead;
