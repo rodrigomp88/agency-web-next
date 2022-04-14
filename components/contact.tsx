940529074ced9e94a2de6e1838740fdd811e4c95
Rodrigo <rodrigomp88@gmail.com>
2022-04-14 14:10:20 -0300

    contact form FormsPree

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,69 @@
 import React, { useState, useCallback } from "react";
 import Image from "next/image";
+import axios from "axios";
 
 const Contact: React.FC = () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+
+  const [inputs, setInputs] = useState({
+    companyName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleOnChange = useCallback((e: any) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  }, []);
+
+  const handleServerResponse = useCallback((ok: any, msg: any) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg },
+      });
+      setInputs({
+        companyName: "",
+        email: "",
+        message: "",
+      });
+    } else {
+      setStatus({
+        submitted: false,
+        submitting: false,
+        info: { error: true, msg },
+      });
+    }
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+      axios({
+        method: "POST",
+        url: process.env.NEXT_PUBLIC_CONTACT_FORM_ENDPOINT_URL,
+        data: inputs,
+      }).then((_response) => {
+        handleServerResponse(true, "Gracias, Tu mensaje fue enviado");
+      });
+    },
+    [inputs, handleServerResponse]
+  );
+
   return (
     <div className="bg-black text-white flex flex-col justify-center items-center pt-10 min-h-screen">
       <div className="flex-1 flex flex-col justify-center items-center pt-10 lg:pt-6"></div>
@@ -9,41 +71,75 @@ const Contact: React.FC = () => {
         <Image src="/assets/logo.svg" width={30} height={35} alt="logo" />
       </div>
       <h2 className="text-4xl font-bold">Contactenos</h2>
-      <form className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w-full lg:min-w-[500px]">
-        <input
-          id="companyName"
-          name="companyName"
-          required
-          maxLength={128}
-          type="text"
-          placeholder="Nombre"
-          className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-2"
-        />
-        <input
-          id="email"
-          name="email"
-          type="email"
-          required
-          maxLength={128}
-          placeholder="Tu correo"
-          className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-2"
-        />
-        <textarea
-          id="message"
-          name="message"
-          required
-          maxLength={1029394}
-          placeholder="Informacion adicional"
-          className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-6 min-h-[16em]"
-        ></textarea>
-        <div className="text-center my-10 ">
-          <button
-            type="submit"
-            className="bg-white text-black rounded-3xl px-8 py-2"
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 mt-16 px-10 lg:mt-20 min-w-full lg:min-w-[500px]"
+      >
+        {status.info.error && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
           >
-            Enviar
-          </button>
-        </div>
+            <strong className="font-bold">Error</strong>:{" "}
+            <span className="block sm:inline">{status.info.msg}</span>
+          </div>
+        )}
+        {status.submitted ? (
+          <div
+            className="text-white ext-xl font-bold px-4 py-3 rounded relative"
+            role="alert"
+          >
+            Tu mensaje fue enviado correctamente. Pronto nos pondremos en
+            contacto
+          </div>
+        ) : (
+          <>
+            <input
+              id="companyName"
+              name="companyName"
+              required
+              maxLength={128}
+              type="text"
+              placeholder="Nombre"
+              className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-2"
+              onChange={handleOnChange}
+              value={inputs.companyName}
+            />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              maxLength={128}
+              placeholder="Tu correo"
+              className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-2"
+              onChange={handleOnChange}
+              value={inputs.email}
+            />
+            <textarea
+              id="message"
+              name="message"
+              required
+              maxLength={1029394}
+              placeholder="Informacion adicional"
+              className="bg-black text-white outline-none border-2 border-white rounded-3xl px-8 py-6 min-h-[16em]"
+              onChange={handleOnChange}
+              value={inputs.message}
+            ></textarea>
+            <div className="text-center my-10 ">
+              <button
+                type="submit"
+                className="bg-white text-black rounded-3xl px-8 py-2"
+              >
+                {!status.submitting
+                  ? !status.submitted
+                    ? "Submit"
+                    : "Submitted"
+                  : "Submitting..."}
+              </button>
+            </div>
+          </>
+        )}
       </form>
     </div>
   );
